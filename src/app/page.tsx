920af6c6d71969daa70c82a811dfd1150f553318
jay rai <jayrai3003@gmail.com>
2024-06-19 15:33:14 +0530

    updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ import Link from "next/link";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import products from "@/data/products_data.json";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { IoMdMail } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
 
 export default function Home() {
   const settings = {
@@ -239,13 +244,13 @@ export default function Home() {
           <div className="flex flex-row-reverse mt-10">
             <div className="left justify-center w-1/2 pr-20 flex flex-col gap-y-6">
               <div className="">
-                <h1 className="uppercase text-5xl text-right font-bold">
+                <h1 className="uppercase text-5xl font-bold">
                   our
                   <span className="text-themeCarrot"> mission</span>
                 </h1>
-                <div className="h-1 w-[16%] bg-black mt-4 ml-auto"></div>
+                <div className="h-1 w-[16%] bg-black mt-4"></div>
               </div>
-              <p className="text-right">
+              <p className="">
                 Our headquarters and the manufacturing unit are located in
                 Ahmedabad, Gujarat, India.Since our establishment, we believed
                 in challenging ourselves to extend our reach to the
@@ -337,27 +342,122 @@ export default function Home() {
           <div className="h-1 w-[8%] bg-black mt-4"></div>
         </div>
 
-        <div className="">
+        <div className="mt-16">
           <Slider {...settingProducts}>
-            <div>
-              <div>Box 1</div>
-            </div>
-            <div>
-              <div>Box 2</div>
-            </div>
-            <div>
-              <div>Box 3</div>
-            </div>
-            <div>
-              <div>Box 4</div>
-            </div>
-            <div>
-              <div>Box 5</div>
-            </div>
-            <div>
-              <div>Box 6</div>
-            </div>
+            {products.products.map((item, index) => (
+              <div className="border-8 border-[#f4f4f4]">
+                <div
+                  className="card w-full shadow-md bg-themeCarrot"
+                  key={index}
+                >
+                  <Image
+                    src={item.imgUrl}
+                    alt="machine-image"
+                    height={350}
+                    width={350}
+                    className="mx-auto"
+                  />
+                  <div className="bg-white capitalize font-semibold">
+                    <Link
+                      href={item.slug}
+                      className="flex items-center gap-x-4 px-4 py-4"
+                    >
+                      {item.name} <FaLongArrowAltRight />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
           </Slider>
+        </div>
+      </div>
+
+      {/* contact section   */}
+      <div className="contact bg-white p-24" id="contact">
+        <div className="">
+          <div className="font-semibold text-[#4a4b54] uppercase tracking-wide mb-3">
+            contact us
+          </div>
+          <h1 className="uppercase text-5xl font-bold">
+            get in <span className="text-themeCarrot">touch</span>
+          </h1>
+          <div className="h-1 w-[12%] bg-black mt-4"></div>
+        </div>
+
+        <div className="flex mt-12">
+          <div className="left w-1/2 flex flex-col gap-y-8">
+            <div className="flex gap-x-4">
+              <div className="text-2xl">
+                <IoMdMail />
+              </div>
+              <div className=" text-[#4a4b54]">raijay2003@gmail.com</div>
+            </div>
+
+            <div className="flex gap-x-4">
+              <div className="text-2xl">
+                <IoCall />
+              </div>
+              <div className=" text-[#4a4b54]">+91 6351468706</div>
+            </div>
+
+            <div className="flex gap-x-4">
+              <div className="text-2xl">
+                <FaLocationDot />
+              </div>
+              <div className="mr-[220px] text-[#4a4b54]">
+                Plot No.24/25-1, Zaveri Ind. Estate, Opp. Shubh Estate Gate,
+                Kathwada – Singarva Road, Kathwada, Ahmedabad-382430. Gujarat -
+                INDIA.
+              </div>
+            </div>
+          </div>
+          <div className="right w-1/2">
+            <form
+              action="#"
+              method="post"
+              className="text-[#4a4b54] flex flex-col gap-y-6"
+            >
+              <div className="flex flex-col">
+                <label htmlFor="name" className="capitalize text-lg">
+                  your name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="border-b-2 border-[#4a4b54] w-[80%] mt-2"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="capitalize text-lg">
+                  your email
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="border-b-2 border-[#4a4b54] w-[80%] mt-2"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="message" className="capitalize text-lg">
+                  message
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={5}
+                  className="border-b-2 border-[#4a4b54] w-[80%] mt-2"
+                  cols={10}
+                />
+              </div>
+              <Link href={"/about-us"} className="mt-0">
+                <button className="uppercase text-white bg-themeCarrot px-6 font-semibold py-2">
+                  submit
+                </button>
+              </Link>
+            </form>
+          </div>
         </div>
       </div>
     </main>
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from "react";
+
+function Footer() {
+  return <div className="bg-black text-white pt-16 px-24 pb-10">Footer</div>;
+}
+
+export default Footer;
